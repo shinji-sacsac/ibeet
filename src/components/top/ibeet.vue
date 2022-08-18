07363f071de69c1df9323c85867c7997e1920b30
shinji omura <shinji@sacsac.co.jp>
2022-08-18 13:02:05 +0900

    add: 会社情報リンク

--- src/components/top/ibeet.vue
+++ src/components/top/ibeet.vue
@@ -1,15 +1,24 @@
 <template>
-    <div />
+    <div class="pt-4">
+        <h1 class="text-left pt-6">
+            会社情報はこちら
+        </h1>
+        <div class="col">
+            <div class="col-sm-2">
+                <a href="https://www.ibeet.co.jp/" target="_blank">https://www.ibeet.co.jp/</a>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-export default {
-    props: {
-        faq: {
-            type: Array,
-            required: false,
-            default: () => []
-        }
-    }
-};
+// export default {
+//     props: {
+//         faq: {
+//             type: Array,
+//             required: false,
+//             default: () => []
+//         }
+//     }
+// };
 </script>
