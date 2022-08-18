aef57e5f4bdedbcddb898ff1ddacbc910589e9e2
shinji omura <shinji@sacsac.co.jp>
2022-08-18 16:58:07 +0900

    add: faqプレビュー追加

---
+++ src/pages/faq_preview/index.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h1 class="title c-text_bold">
+            {{ response.details.subject }}
+        </h1>
+        <div class="post">
+            <dl v-for="(item, idx) in response.details.faq_group_q" :key="idx" class="faqlist">
+                <dt>{{ item }}</dt>
+                <dd>{{ response.details.faq_group_a[idx] }}</dd>
+            </dl>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    async asyncData({ $axios, route }) {
+        return {
+            response: await $axios.$get('/rcms-api/4/faq/preview', {
+                params: {
+                    preview_token: route.query.preview_token
+                }
+            })
+        };
+    }
+};
+</script>
