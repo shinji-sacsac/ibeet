ad2ce808b92b517cb82dc4aa9e44cf2c5bee507a
shinji omura <shinji@sacsac.co.jp>
2022-08-18 13:02:51 +0900

    fix: 会社情報リンク

--- src/components/top/ibeet.vue
+++ src/components/top/ibeet.vue
@@ -3,10 +3,8 @@
         <h1 class="text-left pt-6">
             会社情報はこちら
         </h1>
-        <div class="col">
-            <div class="col-sm-2">
-                <a href="https://www.ibeet.co.jp/" target="_blank">https://www.ibeet.co.jp/</a>
-            </div>
+        <div>
+            <a href="https://www.ibeet.co.jp/" target="_blank">https://www.ibeet.co.jp/</a>
         </div>
     </div>
 </template>
